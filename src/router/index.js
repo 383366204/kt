9e97e7b98bcdb55a5ac2b3bf58615a7509ddc3d4
suen <383366204@qq.com>
2017-10-25 09:18:34 +0800

    cart init

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/views/Index'
 import MyDesign from '@/views/myDesign'
-
+import Cart from '@/views/cart'
 
 
 Vue.use(Router)
@@ -10,12 +10,18 @@ Vue.use(Router)
 export default new Router({
     mode: 'history',
     routes: [{
-        path: '/',
-        name: 'Index',
-        component: Index
-    }, {
-        path: '/MyDesign',
-        name: 'MyDesign',
-        component: MyDesign
-    }]
+            path: '/',
+            name: 'Index',
+            component: Index
+        }, {
+            path: '/MyDesign',
+            name: 'MyDesign',
+            component: MyDesign
+        },
+        {
+            path: '/Cart',
+            name: 'Cart',
+            component: Cart
+        }
+    ]
 })
---
+++ src/views/cart.vue
@@ -0,0 +1,26 @@
+<template>
+ <h1><i class="iconfont icon-gouwuche"></i> 这里是购物车</h1>
+</template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    methods: {
+
+    }
+  }
+</script>
+<style scoped>
+    h1{
+        font-size: 20px;
+    }
+    .icon-gouwuche{
+        font-size: 30px;
+        color: #2eb4e9;
+    }
+</style>

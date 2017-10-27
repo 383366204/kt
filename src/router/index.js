4182277f032da2b756e473ff8671b2eead50e953
wyq_ <840980261@qq.com>
2017-10-27 12:14:25 +0800

    Address 地址管理  by wyq

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import Index from '@/views/Index'
 import MyDesign from '@/views/myDesign'
 import Cart from '@/views/cart'
+import Address from '@/views/Address'
+import order from '@/views/order'
 
 
 Vue.use(Router)
@@ -22,6 +24,16 @@ export default new Router({
             path: '/Cart',
             name: 'Cart',
             component: Cart
+        },
+        {
+            path: '/Address',
+            name: 'Address',
+            component: Address
+        },
+        {
+            path: '/order',
+            name: 'order',
+            component: order
         }
     ]
 })
